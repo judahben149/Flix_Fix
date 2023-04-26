f87b4192d3028072c55e67869302e36769f62ceb
Judah <judahben149@gmail.com>
2023-04-26 15:32:49 +0100

    Added app wide constants

--- app/src/main/java/com/judahben149/flixfix/utils/Constants.kt
+++ app/src/main/java/com/judahben149/flixfix/utils/Constants.kt
@@ -5,4 +5,7 @@ object Constants {
     const val API_KEY = "30239e3691bfcb7c062c8383a63cf7a9"
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/"
+    const val NETWORK_PAGE_SIZE = 20
+
+    const val TAG = "meme"
 }
