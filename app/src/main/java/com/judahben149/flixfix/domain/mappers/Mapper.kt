8569ba5b8c863f261d6ea18834a779d113408652
Judah <judahben149@gmail.com>
2023-05-05 19:06:17 +0100

    Added mapping functionality from DB entities to domain models

--- app/src/main/java/com/judahben149/flixfix/domain/mappers/Mapper.kt
+++ app/src/main/java/com/judahben149/flixfix/domain/mappers/Mapper.kt
@@ -1,13 +1,23 @@
 package com.judahben149.flixfix.domain.mappers
 
+import com.judahben149.flixfix.data.local.entity.MovieEntity
+import com.judahben149.flixfix.data.local.entity.MovieListEntity
 import com.judahben149.flixfix.data.remote.response.DiscoverMoviesDataDto
 import com.judahben149.flixfix.data.remote.response.MovieDto
-import com.judahben149.flixfix.domain.entity.Movie
-import com.judahben149.flixfix.domain.entity.MovieDetailedEntity
+import com.judahben149.flixfix.domain.entity.MovieListModel
+import com.judahben149.flixfix.domain.entity.MovieModel
 
 interface Mapper {
 
-    fun toMovieListModel(moviesDataDto: DiscoverMoviesDataDto): Movie
+    //to remove
+    fun dtoToMovieListModel(moviesDataDto: DiscoverMoviesDataDto): MovieListModel
 
-    fun toMovieDetailedModel(movieDto: MovieDto): MovieDetailedEntity
+    fun entityToMovieListModel(movieListEntity: MovieListEntity): MovieListModel
+
+    fun toMovieListDBEntity(moviesDataDto: DiscoverMoviesDataDto): MovieListEntity
+
+    //to remove
+    fun toMovieModel(movieDto: MovieDto): MovieModel
+
+    fun toMovieDBEntity(movieDto: MovieDto): MovieEntity
 }
