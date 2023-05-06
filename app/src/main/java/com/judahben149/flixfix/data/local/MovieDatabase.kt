e92476990178b1eeed3e17b223a5a2afc489a7cf
Judah <judahben149@gmail.com>
2023-05-06 13:33:01 +0100

    Set database export schema to false

--- app/src/main/java/com/judahben149/flixfix/data/local/MovieDatabase.kt
+++ app/src/main/java/com/judahben149/flixfix/data/local/MovieDatabase.kt
@@ -6,7 +6,7 @@ import com.judahben149.flixfix.data.local.entity.MovieEntity
 import com.judahben149.flixfix.data.local.entity.MovieEntityRemoteKey
 import com.judahben149.flixfix.data.local.entity.MovieListEntity
 
-@Database(entities = [MovieListEntity::class, MovieEntityRemoteKey::class], version = 1)
+@Database(entities = [MovieListEntity::class, MovieEntityRemoteKey::class], version = 1, exportSchema = false)
 abstract class MovieDatabase: RoomDatabase() {
 
     abstract fun movieDao(): MovieDao
